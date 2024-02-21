5fc63432e1d64b39fb2c32654e018a10cd217cc0
Michelazani <michi.zani220@gmail.com>
2024-02-21 18:29:40 +0100

    added scss

---
+++ vite-boolfolio/src/App.vue
@@ -0,0 +1,11 @@
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1>ciao</h1>
+</template>
+
+<style lang="scss">
+@use './style/general.scss' as *;
+</style>
